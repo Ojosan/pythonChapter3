df4a335c41acea0b918759d9e2d6e908cd90f292
Ojosan <koshka.slytherin@gmail.com>
2020-08-10 17:06:27 -0400

    Create insertcoolname.py

---
+++ insertcoolname.py
@@ -0,0 +1,69 @@
+magic_num = (7%4)**3*10+30+33
+
+print(f"{magic_num}")
+
+reyCC = 10
+finnCC= 18
+
+print (f"rey has more CC than finn. this is {reyCC <=finnCC}.")
+
+
+tomCC = 50
+jerryCC = "50"
+
+print(f"tom doesnt have the same ammount of CC as jerry. this is {tomCC != jerryCC}")
+
+trinCC = 3
+neoCC = 3
+
+print(f"neo says he has the same number of CC as trin. this is {neoCC == trinCC}")
+
+kikiCC = 30
+gigiCC = 31
+print(f"kiki has less CC than gigi. this is {kikiCC < gigiCC}")
+
+bernardCC = 1010
+elsieCC = 1010
+
+print(f"bernard has at least the same ammount of CC as elsie. this is {bernardCC >= elsieCC}")
+
+#pie party i guess :/
+
+total = 124
+grhamCrackerCrustFans = 40
+vanillaWaferCrustFans = 64
+oreoCrustFans = 20
+
+#pie types
+
+#choco + carmel pie
+CCpieCrust = "grham Cracker"
+CCpieSlices = 10
+
+#3x berry pie
+BERRYpieCrust = "vanilla wafer"
+BERRYpieSlices = 12
+
+#pumpkin pie
+PUMPKINpieCrust = "grham cracker"
+PUMPKINpieSlices = 12
+
+#apple pie
+APPLEpieCrust = "vanilla wafer"
+APPLEpieSlices = "10"
+
+#bannana cream pie
+BANNANApieCrust = "vanilla wafer"
+BANNANApieSlices = 10
+
+#mango pie
+MANGOpieCrust = "grham cracker"
+MANGOpieslices = 12
+
+#s'mores pie
+SMOREpieCrust = "oreo"
+SMOREpieslices = 12
+
+print(f"chocomel can be evenly devided for all grham crcker lovers. this is {CCpieSlices/grhamCrackerCrustFans == int}")
+
+print("ok")
